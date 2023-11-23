659b42c2b639246ef8536f2b308ac7862cb92f88
Ivan Krajtmajer <68166336+Ivanlol04@users.noreply.github.com>
2023-11-23 11:08:51 +0100

    Update step5Display.m

--- step5Display.m
+++ step5Display.m
@@ -4,18 +4,20 @@ clear
 %turn data in an array with subfields guess for the player input(every number stored separately),
 % win thatcontains the status of the game, black for number of black peg feedback
 %and same for white
+
+
 function displayGame(game)
 
-turn=length(game);
-win=game(turn).win;
+turn=game.nGuesses;
+win=verifyCode(game);%%works only if verifyCode function returns either a one or a zero 
 
 clc
 
     fprintf("*\t*\t*\t*\t--\tbk\twh\n")
 
     for i=1:1:turn
-        fprintf("%i\t%i\t%i\t%i\t--\t%i\t%i\n",game(i).guess(1),game(i).guess(2) ...
-            ,game(i).guess(3),game(i).guess(4),game(i).black,game(i).white)
+        fprintf("%i\t%i\t%i\t%i\t--\t%i\t%i\n",game.board(i,1),game.board(i,2) ...
+            ,game.board(i,3),game.board(i,4),game.board(i,5),game.board(i,6))
     end
 
     if win==1
@@ -25,7 +27,7 @@ clc
         end
         fprintf("Congratulations! Your score is %i",score)
     elseif win==0 && turn==10
-        fprintf("You ran out of guesses and lost the game :(")
+        fprintf("You ran out of guesses and lost the game, the secret code was %i",game.secretCode)
     else
         for i=turn:1:10
             fprintf("-\t-\t-\t-\t--\t-\t-\n")
@@ -33,4 +35,3 @@ clc
     end
 
 end
-
